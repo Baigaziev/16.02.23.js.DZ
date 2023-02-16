433242495a3401faa1dc72b0e80a70f6ff8aa4b3
Erzhan Baigaziev <albertto_dourio@mail.ru>
2023-02-16 11:44:55 +0600

    Created a function called getStringLength. after through return returned a string and added length- to get the length of the string. Created a variable with the value "Mystring" and assigned it the string "Assalamu Alaikum". after that I output it through the console and got the length of the string Mystring

--- index.js
+++ index.js
@@ -3,3 +3,23 @@ function printName() {
   console.log("Baigaziev Erzhan");
 }
 printName();
+
+//*Напишите функцию, которая выводит объем конуса по заданным параметрам - высота (height) и радиус (radius).
+
+// function getVolume(height, radius) {
+//   let volume = (1 / 3) * Math.PI * Math.pow(radius, 2) * height;
+//   console.log(`Объем конуса равен. ${volume}`);
+//   return volume;
+// }
+
+// getVolume(10, 5);
+ 
+
+//?Напишите функцию, которая принимает строку и возвращает ее длину.
+
+function getStringLength(str) {
+  return str.length;
+}
+let Mystring = `Ассаламу Алейкум`;
+let length = getStringLength(Mystring);
+console.log(`Длинна строки: "${Mystring}" равна ${length}`)
